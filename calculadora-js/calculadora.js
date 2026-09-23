6d59c78b274e286c3ca4d96e93047efc0447678c
Isaac Cazales Merino <isaac_cazales.m@hotmail.com>
2026-09-23 00:24:47 -0600

    Agrego carpeta calculadora-js

---
+++ calculadora-js/calculadora.js
@@ -0,0 +1,34 @@
+//Paso 1: Solicitar los números
+let numero1 = Number(prompt("Ingresa el primer número:"));
+let numero2 = Number(prompt("Ingresa el segundo número:"));
+
+// operaciones
+let suma = numero1 + numero2;
+let resta = numero1 - numero2;
+let multiplicacion = numero1 * numero2;
+let division = numero1 / numero2;
+
+// Mostrar los resultados
+console.log("Suma: " + suma);
+console.log("Resta: " + resta);
+console.log("Multiplicación: " + multiplicacion);
+console.log("División: " + division);
+
+
+//Reto extra
+/*
+let nombre = prompt("¿Cómo te llamas?");
+let numero1 = Number(prompt("Ingresa el primer número:"));
+let numero2 = Number(prompt("Ingresa el segundo número:"));
+
+let suma = numero1 + numero2;
+let resta = numero1 - numero2;
+let multiplicacion = numero1 * numero2;
+let division = numero1 / numero2;
+
+console.log("Hola " + nombre + "!");
+console.log("Suma: " + suma);
+console.log("Resta: " + resta);
+console.log("Multiplicación: " + multiplicacion);
+console.log("División: " + division);
+*/
